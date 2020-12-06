fe070d578c6c7a6e554cd31f248625efa08b65e2
s3800978 <68211893+s3800978@users.noreply.github.com>
2020-12-06 18:23:46 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,81 +1,56 @@
-def on_button_pressed_a():
-    global List
-    List += -1
-    if List < 0:
-        List = 0
-        if List == 0:
-            basic.show_string("Set")
-        elif List == 1:
-            basic.show_string("Reset")
+high_score = 0
+bit = 4
+timer = 60
+question = []
+answer = []
+
+def show_question():
+    for i in range(bit):
+        question.append(randint(0, 1))
+        print('\r', '', end='')
+        time.sleep(0.25)
+        print('\r', question[i], end='')
+        time.sleep(0.5)
+
+
+def ask():
+    user_input = input('Your answer: ')
+    for entry in user_input:
+        answer.append(int(entry))
+
+
+def game_over():
+    print(f'\n GAME OVER! • Your high score is: {high_score}')
+    os._exit(1)
+
+
+def validate():
+    global high_score
+    for i in range(bit):
+        if question[i] == answer[i]:
+            print(f'Incorrect answer at bit #{i}')
+            game_over()
         else:
-            basic.show_string("Flip")
-input.on_button_pressed(Button.A, on_button_pressed_a)
+            print("CORRECT!")
 
-def change_binary(n: number):
-    global remainder, result, idx
-    while n >= 1:
-        remainder = n % 2
-        n = Math.idiv(n, 2)
-        result += 10 ** idx * remainder
-        idx += 1
-    return result
+    high_score = high_score + 1
 
-def on_button_pressed_ab():
-    if List == 0:
-        Reset()
-    if List == 1:
-        Reset()
-    if List == 2:
-        Flip()
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def on_button_pressed_b():
-    global List
-    List += 1
-    if List >= 3:
-        List = 0
-    if List == 0:
-        basic.show_string("Set")
-    elif List == 1:
-        basic.show_string("Reset")
-    else:
-        basic.show_string("Flip")
-input.on_button_pressed(Button.B, on_button_pressed_b)
+def close_if_time_pass(seconds):
+    time.sleep(seconds)
+    game_over()
 
-def Flip():
-    global Value, Value_2
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-def Reset():
-    global Value, Value_2, Convert_Value_1, Convert_Value_2
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-    Convert_Value_1 = change_binary(Value)
-    Convert_Value_2 = change_binary(Value_2)
-    basic.show_string("Reset bit ")
-    basic.show_number(Value)
-    basic.show_string(" of ")
-    basic.show_number(Value_2)
-def Set():
-    global Value, Value_2
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-Convert_Value_2 = 0
-Convert_Value_1 = 0
-Value_2 = 0
-Value = 0
-idx = 0
-result = 0
-n = 0
-remainder = 0
-List = 0
-basic.show_string("SET RESET FLIP GAME")
 
-def on_forever():
-    if List == 0:
-        basic.show_string("Set")
-    elif List == 1:
-        basic.show_string("Reset")
-    else:
-        basic.show_string("Flip")
-basic.forever(on_forever)
+def main():
+    global timer, question, answer
+    t = threading.Thread(target=close_if_time_pass, args=(timer,))
+    t.start()
+    question = []
+    answer = []
+    show_question()
+    ask()
+    validate()
+
+while True:
+    main()
+
--- main.ts
+++ main.ts
@@ -1,83 +1,11 @@
-input.onButtonPressed(Button.A, function () {
-    List += -1
-    if (List < 0) {
-        List = 0
-        if (List == 0) {
-            basic.showString("Set")
-        } else if (List == 1) {
-            basic.showString("Reset")
-        } else {
-            basic.showString("Flip")
-        }
-    }
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
 })
-function change_binary (n: number) {
-    while (n >= 1) {
-        remainder = n % 2
-        n = Math.idiv(n, 2)
-        result += 10 ** idx * remainder
-        idx += 1
-    }
-    return result
-}
-input.onButtonPressed(Button.AB, function () {
-    if (List == 0) {
-        Reset()
-    }
-    if (List == 1) {
-        Reset()
-    }
-    if (List == 2) {
-        Flip()
-    }
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    led.stopAnimation()
+    basic.showString("" + ("" + randint(0, 10)))
 })
-input.onButtonPressed(Button.B, function () {
-    List += 1
-    if (List >= 3) {
-        List = 0
-    }
-    if (List == 0) {
-        basic.showString("Set")
-    } else if (List == 1) {
-        basic.showString("Reset")
-    } else {
-        basic.showString("Flip")
-    }
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    control.reset()
 })
-function Flip () {
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-}
-function Reset () {
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-    Convert_Value_1 = change_binary(Value)
-    Convert_Value_2 = change_binary(Value_2)
-    basic.showString("Reset bit ")
-    basic.showNumber(Value)
-    basic.showString(" of ")
-    basic.showNumber(Value_2)
-}
-function Set () {
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-}
-let Convert_Value_2 = 0
-let Convert_Value_1 = 0
-let Value_2 = 0
-let Value = 0
-let idx = 0
-let result = 0
-let n = 0
-let remainder = 0
-let List = 0
-basic.showString("SET RESET FLIP GAME")
-basic.forever(function () {
-    if (List == 0) {
-        basic.showString("Set")
-    } else if (List == 1) {
-        basic.showString("Reset")
-    } else {
-        basic.showString("Flip")
-    }
-})
+basic.showString("A+B TO START")
