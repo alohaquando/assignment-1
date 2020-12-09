ef1c9202f24e6f27389fab02dd3d1092131f73dc
Taesung00 <62132251+Taesung00@users.noreply.github.com>
2020-12-09 15:24:31 +0700

    Reset_function / edit the values in the function

--- main.ts
+++ main.ts
@@ -1,77 +1,25 @@
-input.onButtonPressed(Button.A, function () {
-    Answer = 1
-})
-function change_binary (n: number) {
-    while (n >= 1) {
-        remainder = n % 2
-        n = Math.idiv(n, 2)
-        result += 10 ** idx * remainder
-        idx += 1
+function reset_list () {
+    answer = []
+    for (let index = 0; index <= answer.length; index++) {
+        basic.pause(100)
     }
-    return result
 }
-input.onButtonPressed(Button.AB, function () {
-    let List_ = 0
-    if (List_ == 0) {
-        Reset()
-    }
-    if (List_ == 1) {
-        Reset()
-    }
-    if (List_ == 2) {
-        Flip()
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    Answer = 1
-})
-function Flip () {
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-}
-function Reset () {
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-    Convert_Value_1 = change_binary(Value)
-    Convert_Value_2 = change_binary(Value_2)
-    basic.showString("WHAT IS THE BINARY VALUE OF")
-    basic.showNumber(Value)
-    if (Answer == change_binary(Value)) {
-        basic.showString("WHAT IS THE RESET VALUE WITH ")
-        basic.showNumber(Convert_Value_2)
-        if (true) {
-            for (let Convert_Value_1 = 0; Convert_Value_1 <= Convert_Value_1.length - 1; Convert_Value_1++) {
-                Length_Reset += 1
-                if (Convert_Value_1[Length_Reset] == Convert_Value_2[Length_Reset]) {
-                    result_reset = Convert_Value_1[Length_Reset]
-                } else {
-                    result_reset = 0
-                }
+function Reset_function (num: number, num2: number) {
+    Compare_edit = [1]
+    result_reset = []
+    for (let index = 0; index <= answer.length; index++) {
+        if (answer[index] == Compare_edit[index]) {
+            if (answer[index] == 1) {
+                result_reset.push(1)
+            } else {
+                result_reset.push(0)
             }
         } else {
-            game.gameOver()
+            result_reset.push(0)
         }
-    } else {
-        game.gameOver()
     }
 }
-function Set () {
-    Value = randint(0, 10)
-    Value_2 = randint(0, 10)
-}
-let Length_Reset = 0
-let Convert_Value_2 = 0
-let Convert_Value_1 = 0
-let Value_2 = 0
-let Value = 0
-let idx = 0
-let result = 0
-let n = 0
-let remainder = 0
-let Answer = 0
-let result_reset = ""
-result_reset = ""
-basic.showString("SET RESET FLIP GAME")
-basic.forever(function () {
-	
-})
+let result_reset: number[] = []
+let Compare_edit: number[] = []
+let answer: number[] = []
+reset_list()
