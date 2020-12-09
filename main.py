e674079faace2c9e7d585f58c1492399059a869f
Taesung00 <62132251+Taesung00@users.noreply.github.com>
2020-12-09 15:48:13 +0700

    reset_function and binary_convert function . need to edit the purpose value

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-function reset_list () {
+def reset_list():
+    global answer
     answer = []
-    for (let index = 0; index <= answer.length; index++) {
+    index = 0
+    while index <= len(answer):
         basic.pause(100)
-    }
-}
-let answer: number[] = []
+        index += 1
+def Reset_function(num: number, num2: number):
+    global Compare_edit, result_reset
+    Compare_edit = [1]
+    result_reset = []
+    index2 = 0
+    while index2 <= len(answer):
+        if answer[index2] == Compare_edit[index2]:
+            if answer[index2] == 1:
+                result_reset.append(1)
+            else:
+                result_reset.append(0)
+        else:
+            result_reset.append(0)
+        index2 += 1
+result_reset: List[number] = []
+Compare_edit: List[number] = []
+answer: List[number] = []
 reset_list()
+
+def change_binary(n: number):
+    b = []
+    result_binary = []
+    while n!=0:
+        b.append(n%2)
+        n = n//2
+    for i in range(len(b)-1,-1,-1):
+        result_binary.append(i)
+    return result_binary
--- main.ts
+++ main.ts
@@ -1,25 +1,48 @@
-function reset_list () {
+function reset_list() {
+    
     answer = []
-    for (let index = 0; index <= answer.length; index++) {
+    let index = 0
+    while (index <= answer.length) {
         basic.pause(100)
+        index += 1
     }
 }
-function Reset_function (num: number, num2: number) {
+
+function Reset_function(num: number, num2: number) {
+    
     Compare_edit = [1]
     result_reset = []
-    for (let index = 0; index <= answer.length; index++) {
-        if (answer[index] == Compare_edit[index]) {
-            if (answer[index] == 1) {
+    let index2 = 0
+    while (index2 <= answer.length) {
+        if (answer[index2] == Compare_edit[index2]) {
+            if (answer[index2] == 1) {
                 result_reset.push(1)
             } else {
                 result_reset.push(0)
             }
+            
         } else {
             result_reset.push(0)
         }
+        
+        index2 += 1
     }
 }
-let result_reset: number[] = []
-let Compare_edit: number[] = []
-let answer: number[] = []
+
+let result_reset : number[] = []
+let Compare_edit : number[] = []
+let answer : number[] = []
 reset_list()
+function change_binary(n: number) {
+    let b = []
+    let result_binary = []
+    while (n != 0) {
+        b.push(n % 2)
+        n = Math.idiv(n, 2)
+    }
+    for (let i = b.length - 1; i < -1; i += -1) {
+        result_binary.push(i)
+    }
+    return result_binary
+}
+
