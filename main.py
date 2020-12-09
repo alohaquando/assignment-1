49496f2bb89f133ab5e7ed7afed7b253b1061fec
s3800978 <68211893+s3800978@users.noreply.github.com>
2020-12-09 22:41:21 +0700

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
-high_score = 0
-bit = 4
-timer = 60
-question = []
-answer = []
-
-def show_question():
-    for i in range(bit):
-        question.append(randint(0, 1))
-        print('\r', '', end='')
-        time.sleep(0.25)
-        print('\r', question[i], end='')
-        time.sleep(0.5)
-
-
-def ask():
-    user_input = input('Your answer: ')
-    for entry in user_input:
-        answer.append(int(entry))
-
+def on_button_pressed_a():
+    answer.append(1)
 
 def game_over():
-    print(f'\n GAME OVER! • Your high score is: {high_score}')
-    os._exit(1)
+    print("""
+ GAME OVER! {high_score}""")
+    game.game_over()
 
+def show_question():
+    for i in range(8):
+        question.append(randint(0, 1))
+        print(question[i])
+        control.wait_micros(250000)
+
+def ask():
+    input.button_is_pressed(Button.A)
+    input.button_is_pressed(Button.B)
+
+def on_button_pressed_b():
+    answer.append(0)
 
 def validate():
     global high_score
-    for i in range(bit):
-        if question[i] == answer[i]:
-            print(f'Incorrect answer at bit #{i}')
+    j = 0
+    while j <= bit - 1:
+        if question[j] == answer[j]:
+            print("X #{i}")
             game_over()
         else:
             print("CORRECT!")
-
+        j += 1
     high_score = high_score + 1
 
-
-def close_if_time_pass(seconds):
-    time.sleep(seconds)
-    game_over()
-
-
 def main():
-    global timer, question, answer
-    t = threading.Thread(target=close_if_time_pass, args=(timer,))
-    t.start()
+    global question, answer
     question = []
     answer = []
     show_question()
     ask()
     validate()
 
-while True:
-    main()
+high_score = 0
+answer: List[number] = []
+bit = 0
+question: List[number] = []
+bit = 4
+timer = 60
 
+
+def on_button_pressed_logo():
+    while True:
+        main()
+
+input.logo_is_pressed()
--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     led.stopAnimation()
-    basic.showString("" + ("" + randint(0, 10)))
+    basic.showString("" + (randint(0, 10)))
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function () {
     control.reset()
 })
 basic.showString("A+B TO START")
